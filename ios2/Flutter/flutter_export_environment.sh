da3f72dcddcfd69d97076ba3908b992eb42e217b
GabrielDinizS <95745774+GabrielDinizS@users.noreply.github.com>
2023-07-20 15:52:44 -0300

    Add files via upload

---
+++ ios2/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/macos11/Documents/dev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/macos11/Documents/Flutter/planejando_seu_dinheiro"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=true"
+export "PACKAGE_CONFIG=/Users/macos11/Documents/Flutter/planejando_seu_dinheiro/.dart_tool/package_config.json"
