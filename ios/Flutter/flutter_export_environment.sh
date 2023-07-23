da89bcf49156548addebd43fe18ac1a5775a457d
GabrielDinizS <gabrieldnz12@outlook.com>
2023-07-23 10:29:28 -0300

    ios

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/macos11/Documents/dev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/macos11/Documents/Flutter/planejando_seu_dinheiro"
+export "FLUTTER_APPLICATION_PATH=/Users/macos11/Documents/Flutter/planejando_seu_dinheiro/planeje"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
@@ -10,4 +10,4 @@ export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=true"
-export "PACKAGE_CONFIG=/Users/macos11/Documents/Flutter/planejando_seu_dinheiro/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/macos11/Documents/Flutter/planejando_seu_dinheiro/planeje/.dart_tool/package_config.json"
